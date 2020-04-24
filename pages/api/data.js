1fcd311f70fbef756c0cd19fa7570f3f0d53d1d5
leadis <murdok@mbp-de-leadis.zte.com.cn>
2020-04-23 20:08:09 -0500

    first commit8

--- pages/api/data.js
+++ pages/api/data.js
@@ -2,7 +2,11 @@ export default (req, res) => {
     console.log('recibido1')
     if (req.method === 'POST') {
       // Process a POST request
-      console.log('recibido2')
+      /* res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({ name: 'John Doe' })) */
+      return('OK')
+      
     } else {
       // Handle any other HTTP method
       console.log('No Llego')
